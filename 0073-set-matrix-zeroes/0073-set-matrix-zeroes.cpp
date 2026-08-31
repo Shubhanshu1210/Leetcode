2ff6566c8aa72277788b48b9dcc23c10b682a11f
Shubhanshu1210 <145764465+Shubhanshu1210@users.noreply.github.com>
2026-08-31 22:44:59 +0530

    Time: 0 ms (100%), Space: 21 MB (27.01%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int n = matrix.size(); 
+        int n = matrix.size();
         int m = matrix[0].size();
+        vector<int> arow(m, 1);
+        vector<int> acol(n, 1);      
+        
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(matrix[i][j]==0){
-                    for(int k=0;k<m;k++){
-                        if(matrix[i][k]!=0){
-                            matrix[i][k]=-545;
-                        }
-                    }
-                    for(int k=0;k<n;k++){
-                        if(matrix[k][j]!=0){
-                            matrix[k][j]=-545;
-                        }
-                    }
+                    arow[j]=0;
+                    acol[i]=0;
                 }
             }
         }
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(matrix[i][j]==-545){
-                    matrix[i][j]=0;
+                if(matrix[i][j]!=0){
+                    if(arow[j]==0 || acol[i]==0){
+                        matrix[i][j]=0;
+                    }
                 }
             }
         }
