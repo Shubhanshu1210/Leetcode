063ef5e967a8187f0ab6624a5c6409950d0a52af
Shubhanshu1210 <145764465+Shubhanshu1210@users.noreply.github.com>
2026-09-19 20:27:52 +0530

    Time: 1115 ms (9.71%), Space: 23 MB (24.74%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,23 +1,37 @@
 class Solution:
-    def f(self, index, T, nums, dp):
+    def f(self, index, T, coins, dp):
         if dp[index][T] != -1:
                 return dp[index][T]
         if(index == 0):
-            if T % nums[0] == 0:
-                return T // nums[0]
+            if T % coins[0] == 0:
+                return T // coins[0]
             return 1e9
             if dp[index][T] != -1:
                 return dp[index][T]
-        nottake = 0 + self.f(index-1, T, nums, dp)
+        nottake = 0 + self.f(index-1, T, coins, dp)
         take = float('inf')
-        if nums[index] <= T:
-            take = 1 + self.f(index, T-nums[index], nums, dp)
+        if coins[index] <= T:
+            take = 1 + self.f(index, T-coins[index], coins, dp)
         dp[index][T] = min(take, nottake)
         return dp[index][T]
         
     def coinChange(self, coins: list[int], amount: int) -> int:
-        dp = [[-1] * (amount + 1) for _ in range(len(coins))]
-        ans = self.f(len(coins)-1, amount, coins, dp)
+        dp = [[0] * (amount + 1) for _ in range(len(coins))]
+        for i in range(amount+1):
+            if i % coins[0] == 0:
+                dp[0][i] = i // coins[0]
+            else:
+                dp[0][i] = 1e9
+        for index in range(1,len(coins)):
+            for T in range(amount+1):
+                nottake = 0 + dp[index - 1][T]
+                take = float('inf')
+                if coins[index] <= T:
+                    take = 1 + dp[index][T-coins[index]]
+                dp[index][T] = min(take, nottake)
+                
+                
+        ans = dp[len(coins)-1][amount]
         if(ans >= 1e9):
             return -1
         return ans
